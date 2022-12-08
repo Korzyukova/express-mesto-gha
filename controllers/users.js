799181e4f4dfa5ad44bfa4d106a1f2ba49071eeb
Tatiana Korzyukova <korzioukova@gmail.com>
2022-12-08 01:42:56 -0500

    patch

--- controllers/users.js
+++ controllers/users.js
@@ -76,7 +76,7 @@ module.exports.updateUser = (req, res) => {
     update.avatar = avatar;
   }
 
-  User.updateOne({ _id: req.body.userId }, update)
+  User.updateOne({ _id: req.user._id }, update)
     .then((user) => res.send({ data: user }))
     .catch(() =>
       res.status(500).send({ message: "Запрашиваемый пользователь не найден" })
@@ -87,7 +87,7 @@ module.exports.updateUserAvatar = (req, res) => {
   if (!req.body.avatar) {
     res.status(400).send({ message: "Запрашиваемый пользователь не найден" });
   }
-  User.updateOne({ _id: req.body.userId }, { avatar: req.body.avatar })
+  User.updateOne({ _id: req.user._id }, { avatar: req.body.avatar })
     .then((user) => res.send({ data: user }))
     .catch(() =>
       res.status(500).send({ message: "Запрашиваемый пользователь не найден" })
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
-const { createUser, getUsers, getUserId, updateUser, updateUserAvatar} = require("../controllers/users");
+const {
+  createUser,
+  getUsers,
+  getUserId,
+  updateUser,
+  updateUserAvatar,
+} = require("../controllers/users");
 
 router.get("/users", getUsers);
 router.post("/users", createUser);
+router.patch("/users/me", updateUser);
+router.patch("/users/me/avatar", updateUserAvatar);
 router.get("/users/:userId", getUserId);
-
-router.patch("users/me", updateUser);
-router.patch("users/me/avatar", updateUserAvatar);
 
 module.exports = router;
