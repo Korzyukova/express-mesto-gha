d2592b44656ffc356f994b35d038c008015a5faa
Tatiana Korzyukova <korzioukova@gmail.com>
2022-12-23 18:21:49 -0500

    14-20

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const auth = require('../middlewares/auth');
 
@@ -12,8 +13,24 @@ const {
 
 router.get('/users', auth, getUsers);
 router.get('/users/me', auth, getMe);
-router.patch('/users/me', auth, updateUser);
-router.patch('/users/me/avatar', auth, updateUserAvatar);
-router.get('/users/:userId', auth, getUserId);
+router.patch('/users/me', auth, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    password: Joi.string(),
+    email: Joi.string().email().pattern(/^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i).uri(),
+  }),
+}), updateUser);
+router.patch('/users/me/avatar', auth, celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i).uri(),
+  }),
+}), updateUserAvatar);
+router.get('/users/:userId', auth, celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24),
+  }),
+}), getUserId);
 
 module.exports = router;
