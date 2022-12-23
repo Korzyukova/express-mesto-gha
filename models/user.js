507d33f4217700c44a49ec949c482fcb9e10172a
Tatiana Korzyukova <korzioukova@gmail.com>
2022-12-23 13:44:11 -0500

    14-3

--- models/user.js
+++ models/user.js
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema({
     required: true,
     validate: {
       validate(v) {
-        return /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i.test(v);
+        return /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i.test(v);
       },
       message: (props) => `${props.value} is not a valid email!`,
     },
