ef775a5cbb49515b7969dcef7f7d7356ed544d7c
Tatiana Korzyukova <korzioukova@gmail.com>
2022-12-23 15:50:20 -0500

    14-7

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.post('/users', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     password: Joi.string().required(),
-    email: Joi.string().email().required(),
+    email: Joi.string().email().required().pattern(/^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string(),
   }),
